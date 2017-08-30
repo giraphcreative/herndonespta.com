3d28806e494ae268b9be639fe5531736e659ffdf
James Pederson <james@jpederson.com>
2017-08-30 00:41:54 -0500

    Add events calendar template.

---
+++ page-events.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Template Name: Events Calendar
+*/
+
+get_header();
+
+?>
+
+	<?php the_showcase(); ?>
+
+	<?php the_large_title(); ?>
+	
+	<div id="content" class="wrap group content-wide" role="main">
+		<iframe src="https://calendar.google.com/calendar/embed?src=herndonptamembership%40gmail.com&ctz=America/New_York" border=0 style="width: 100%; height: 500px; border: 0;"></iframe>
+		<hr />
+		<?php 
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); 
+				the_content();
+			endwhile;
+		endif;
+		?>
+	</div><!-- #content -->
+
+<?php
+
+get_footer();
+
+?>
