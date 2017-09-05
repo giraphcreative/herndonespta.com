7cd23e0ce9d1062827ccad3d18acb04d26d5cf15
James Pederson <james@jpederson.com>
2017-09-05 17:21:39 -0500

    Remove the showcase and large title output on the WooCommerce template.

--- woocommerce.php
+++ woocommerce.php
@@ -4,10 +4,6 @@ get_header();
 
 ?>
 
-	<?php the_showcase(); ?>
-
-	<?php the_large_title(); ?>
-	
 	<div id="content" class="wrap group content-wide" role="main">
 		<?php woocommerce_content(); ?>
 	</div><!-- #content -->
