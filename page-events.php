1890b0d10f33313920e92251cc5d737ecf46bd56
James Pederson <james@jpederson.com>
2018-11-07 11:58:16 -0600

    Update calendar URL.

--- page-events.php
+++ page-events.php
@@ -13,7 +13,7 @@ get_header();
 	<?php the_large_title(); ?>
 	
 	<div id="content" class="wrap group content-wide" role="main">
-		<iframe src="https://calendar.google.com/calendar/embed?src=herndonptamembership%40gmail.com&ctz=America/New_York" border=0 style="width: 100%; height: 500px; border: 0;"></iframe>
+		<iframe src="https://calendar.google.com/calendar/embed?src=herndonptasecretary%40gmail.com&ctz=America%2FNew_York" border=0 style="width: 100%; height: 500px; border: 0;"></iframe>
 		<hr />
 		<?php 
 		if ( have_posts() ) :
